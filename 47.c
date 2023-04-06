48be4d6de23b0285ae715545fe624eab340f37a7
이시후 <dltlgn109@gmail.com>
2023-04-06 05:08:40 +0000

    모르겠다

---
+++ 47.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+	int num1=16;
+	int num2;
+	int sum=0;
+	sum = num1 + num2;
+	sub = num1 - num2;
+	mul = num1 * num2;
+	div= num1 / num2;
+	printf ("%d + %d = %d",num1,num2 ,sum);
+	printf ("%d - %d = %d",num1,num2 ,sum);
+	printf ("%d * %d = %d",num1,num2 ,sum);
+	printf ("%d / %d = %d",num1,num2 ,sum);
+	
+	return 0;
+}
